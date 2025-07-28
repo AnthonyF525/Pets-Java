2adf3841931bb79a7341951015068deec2da6e8b
AnthonyF525 <Anthony.falice@yahoo.com>
2025-07-28 12:02:02 -0400

    For Pull request

--- src/main/java/com/zipcodewilmington/assessment1/Pet.java
+++ src/main/java/com/zipcodewilmington/assessment1/Pet.java
@@ -9,12 +9,16 @@ public abstract class Pet implements Animal {
      * by default, pet has age of 0; name of "";
      */
     public Pet() {
+        this.name = "";
+        this.age = 0;
     }
 
     /**
      * @param name name of this pet
      */
     public Pet(String name) {
+        this.name = name;
+        this.age = 0;
     }
 
 
@@ -22,6 +26,8 @@ public abstract class Pet implements Animal {
      * @param age age of this pet
      */
     public Pet(int age) {
+        this.age = age;
+        this.name = "";
     }
 
     /**
@@ -29,20 +35,22 @@ public abstract class Pet implements Animal {
      * @param age age of this pet
      */
     public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     /**
      * @return name of this pet
      */
     public String getName() {
-        return null;
+        return name;
     }
 
     /**
      * @return age of this pet
      */
     public Integer getAge() {
-        return null;
+        return age;
     }
 
     /**
@@ -50,6 +58,7 @@ public abstract class Pet implements Animal {
      * ensure this instance of `Pet` is added to the owner's composite `pets` list
      */
     public void setOwner(PetOwner newPetOwner) {
+        this.petOwner = newPetOwner;
     }
 
     /**
